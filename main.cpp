153b2923c8505997de61dacff4fce27573b9ba20
Tesfaye-Kel <tesfayekelbesa912@gmail.com>
2026-04-14 11:41:29 -0700

    add triangle rendering to OpenG

--- main.cpp
+++ main.cpp
@@ -2,10 +2,28 @@
 #include <GL/glu.h>
 
 void display() {
+    // Clear the buffers (clear color was set during init)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Set clear color (dark blue)
-    glClearColor(0.1f, 0.2f, 0.4f, 1.0f);
+    // Set up modelview and camera
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    // place camera a bit along +Z so we can see objects at the origin
+    gluLookAt(0.0, 0.0, 5.0,
+              0.0, 0.0, 0.0,
+              0.0, 1.0, 0.0);
+
+    // Draw a colored triangle
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-1.0f, -1.0f, 0.0f);
+
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(1.0f, -1.0f, 0.0f);
+
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(0.0f, 1.0f, 0.0f);
+    glEnd();
 
     glFlush();
     glutSwapBuffers();
@@ -28,6 +46,8 @@ void reshape(int w, int h) {
 
 void init() {
     glEnable(GL_DEPTH_TEST);
+    // Set clear color (dark blue) once during initialization
+    glClearColor(0.1f, 0.2f, 0.4f, 1.0f);
 }
 
 int main(int argc, char** argv) {
